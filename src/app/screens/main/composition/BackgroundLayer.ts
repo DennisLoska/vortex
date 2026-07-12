866398514231f9867a9c67d77884258071e94eb9
Dennis Loska <codefree@proton.me>
2026-07-12 15:33:18 +0200

    feat: add background layer

---
+++ src/app/screens/main/composition/BackgroundLayer.ts
@@ -0,0 +1,32 @@
+import { Container, Sprite, Texture, VideoSource } from "pixi.js";
+
+export class BackgroundLayer extends Container {
+  private sprite: Sprite | undefined;
+
+  public async setBackground(texture: Texture) {
+    if (this.sprite) {
+      this.removeChild(this.sprite);
+      this.sprite.destroy();
+      this.sprite = undefined;
+    }
+
+    this.sprite = new Sprite({ texture, anchor: 0.5 });
+    this.addChild(this.sprite);
+    this.resize(this.parent?.width ?? 1920, this.parent?.height ?? 1080);
+  }
+
+  public resize(width: number, height: number) {
+    if (!this.sprite) return;
+
+    const texture = this.sprite.texture;
+    const isVideo = texture.source instanceof VideoSource;
+    if (isVideo) {
+      const video = (texture.source as VideoSource).resource;
+      video?.play?.();
+    }
+
+    const scale = Math.max(width / texture.width, height / texture.height);
+    this.sprite.scale.set(scale);
+    this.sprite.position.set(width * 0.5, height * 0.5);
+  }
+}
